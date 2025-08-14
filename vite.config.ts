94257fe2711167abd68c69473f355a67154aa339
mgpn1 <m.gpn1@mail.com>
2025-08-14 10:11:58 +0530

    gh deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/staytechl1/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
